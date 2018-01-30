dedd138132d1a5cc6b76f4ce7a16d205bfe7a72a
Tiina Autio <suprajohtavuus@gmail.com>
2018-01-30 23:51:59 +0200

    1.6

---
+++ Osa 1/tehtava16/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => 
+
+{
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
